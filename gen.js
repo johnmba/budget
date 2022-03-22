4fb5ec4c4ba710aa67406ea2b9a34c44cb693718
= <=>
2022-03-23 00:07:13 +0100

    sound and working

--- gen.js
+++ gen.js
@@ -1,8 +1,8 @@
 
 function total(total_amt, dated){
     let tot_temp = `<div class="buget-row total">
-    <span>Total</span> <span>Grand total of amount</span>
-    <span>${total_amt}</span> <span>${dated}</span>
+    <div class="unit _1fb">Total</div> <div class="unit _2fb">Grand total of amount</div>
+    <div class="unit _1fb flxc">${total_amt}</div> <div class="unit _1fb">${dated}</div>
     </div>`
     return tot_temp
 }
@@ -11,36 +11,29 @@ function temp(obj){
     
     let temp = `
         <div class="buget-row">
-            <span>${obj.ovahd}</span> <span>${obj.desc}</span>
-            <span>${obj.amt}</span> <span>${obj.dated}</span>
+            <div class="unit _1fb">${obj.ovahd}</div> <div class="unit _2fb">${obj.desc}</div>
+            <div class="unit _1fb flxc">${obj.amt}</div> <div class="unit _1fb">${obj.dated}</div>
         </div>
     `;
     return temp
 }
 
-var savedata = function (name, value) {
-
-	var exists = localStorage.getItem(name);
-	existing = exists ? exists.split(',') : [];
-	existing.unshift(value);
-    JSON.stringify(existing)
-	localStorage.setItem(name, existing);
-    console.log(existing);
-};
-
 const date = new Date();
 var form = document.forms;
 
 var income = document.querySelectorAll("#incomsectn");
-var expense = document.querySelectorAll("#expensectn");
 var revsectn = document.createElement("div");
 var totrev = document.createElement("div");
+var expense = document.querySelectorAll("#expensectn");
 var expsectn = document.createElement("div");
 var totexp = document.createElement("div");
-console.log(localStorage.total_inc)
-var draft = "You have $" + localStorage.total_rev + " in draft for you to spend"
 
 window.addEventListener("load", ()=>{
+
+    if(!sessionStorage.getItem("visit")){
+        localStorage.clear()
+    }
+
     if (localStorage.rev){
         let revenue = localStorage.rev
         revenue = JSON.parse(revenue)
@@ -60,7 +53,8 @@ window.addEventListener("load", ()=>{
     }
     if(localStorage.exp){
 
-        let expenditure = JSON.parse(localStorage.exp);
+        let expenditure = localStorage.exp;
+        expenditure =  JSON.parse(expenditure)
 
         expenditure.forEach(element => {
 
@@ -70,10 +64,10 @@ window.addEventListener("load", ()=>{
             expense[0].appendChild(exp);
         });
         totexp.innerHTML = total(localStorage.total_exp, expenditure[0].dated);
-        expense[0].appendChild(totsectn);
+        expense[0].appendChild(totexp);
     }else if (typeof localStorage.total_exp == "undefined" && typeof localStorage.rev !== "undefined"){
         expsectn.className = "item nobuge";
-        expsectn.innerHTML = draft
+        expsectn.innerHTML = "You have $" + localStorage.total_rev + " in draft for you to spend";
         expense[0].appendChild(expsectn)
     }else{
         form.exp.expensa.disabled = true;
@@ -86,6 +80,7 @@ window.addEventListener("load", ()=>{
 
 form.inc.addEventListener("submit", (e)=>{
     e.preventDefault();
+    sessionStorage.setItem("visit", "true");
 
     var incom = new FormData(e.target);
     incom = Object.fromEntries(incom);
@@ -95,23 +90,24 @@ form.inc.addEventListener("submit", (e)=>{
         localStorage.total_rev = Number(localStorage.total_rev) + Number(incom.amt)
     }else{
         localStorage.total_rev = incom.amt
+        console.log(localStorage.total_rev)
     };
+    
     incom.dated = date.toDateString();
-
     if (localStorage.rev) {
-        localStorage.rev = localStorage.total_rev.unshift(incom)
+        let stored = JSON.parse(localStorage.getItem("rev"))
+        stored.unshift(incom)
+        localStorage.rev = JSON.stringify(stored) 
     }else{
-        emty = []
+        let emty = []
         emty.push(incom)
         localStorage.rev = JSON.stringify(emty)
     };
-    //console.log(savedata("rev", incom));
 
     let norev = document.querySelector("#incomsectn .nobuge");
     if (norev){
         norev.remove()
     };
-    console.log(incom)
 
     let old_total = document.querySelector("#incomsectn .total");
     if(old_total){
@@ -125,7 +121,7 @@ form.inc.addEventListener("submit", (e)=>{
 
     let noexp = document.querySelector("#expensectn .nobuge");
     if (noexp && typeof localStorage.exp == "undefined"){
-        noexp.innerHTML = draft
+        noexp.innerHTML = "You have $" + localStorage.total_rev + " in draft for you to spend, note you cannot spend more than."
     };
     form.exp.expensa.disabled = false;
     window.location.href = "#incomsectn";
@@ -134,15 +130,16 @@ form.inc.addEventListener("submit", (e)=>{
 
 form.exp.addEventListener("submit", (e)=>{
     e.preventDefault();
+    sessionStorage.setItem("visit", "true");
 
     var expens = new FormData(e.target);
     expens = Object.fromEntries(expens);
     const newexp = document.createElement("div")
 
-    if (localStorage.total_rev) {
-        localStorage.total_rev = Number(localStorage.total_rev) + Number(expens.amt);
+    if (localStorage.total_exp) {
+        localStorage.total_exp = Number(localStorage.total_exp) + Number(expens.amt);
     } else {
-        localStorage.total_rev = expens.amt;
+        localStorage.total_exp = expens.amt;
     }
 
     expens.dated = date.toDateString()
@@ -152,11 +149,13 @@ form.exp.addEventListener("submit", (e)=>{
         noexp.remove()
     }
     if (localStorage.exp) {
-        localStorage.exp = Array(localStorage.total_rev).unshift(expens)
+        let stored = JSON.parse(localStorage.getItem("exp"))
+        stored.unshift(expens)
+        localStorage.exp = JSON.stringify(stored) 
     }else{
-        emty = []
+        let emty = []
         emty.push(expens)
-        localStorage.rev = JSON.stringify(emty)
+        localStorage.exp = JSON.stringify(emty)
     };
 
     let old_total = document.querySelector("#expensectn .total");
@@ -166,9 +165,39 @@ form.exp.addEventListener("submit", (e)=>{
 
     newexp.innerHTML = temp(obj=expens);
     expense[0].appendChild(newexp);
-    totexp.innerHTML = total(localStorage.total_rev, expens.dated);
+    totexp.innerHTML = total(localStorage.total_exp, expens.dated);
     expense[0].appendChild(totexp);
     window.location.href = "#expensectn";
-
     return
+})
+
+const expamt = document.getElementById("expamt")
+
+expamt.addEventListener("change", (event)=>{
+    const err = document.createElement("div")
+    err.style.fontSize = "small"
+    err.id = "err"
+    err.style.color = "red"
+    err.style.marginLeft = "60px"
+    err.style.paddingLeft = "10px"
+    err.style.paddingTop = "5px"
+    const val = event.target.value
+    if(!localStorage.total_exp){
+        var spends = 0
+    }else{
+        spends = Number(localStorage.total_exp)
+    }
+    const draft = Number(localStorage.total_rev) - spends
+    
+    if (val > draft) {
+        err.innerHTML = "Overdraft $ " + draft + " is not allowed"
+        expamt.parentElement.appendChild(err)
+        form.exp.expensa.disabled = true
+    } else {
+        let noerr = document.getElementById("err")
+        if (noerr) {
+            noerr.remove()
+        }
+        form.exp.expensa.disabled = false
+    }
 })
